b8bb6b56dad52a18a145d2189fcb9d7e87cd99a6
Bar Michaeli <bbarmic144@gmail.com>
2025-05-12 19:57:44 +0300

    סיום העברת צבעים למשתנים

--- services/global.js
+++ services/global.js
@@ -11,8 +11,19 @@ function darkMode() {
   if (body.className === darkModeClass) 
   {
     body.className = '';
+    localStorage.setItem('theme', 'light');
   } 
   else 
   {
     body.className = darkModeClass;
-  }}
+    localStorage.setItem('theme', 'dark');
+  }
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  const savedTheme = localStorage.getItem('theme');
+  if (savedTheme === 'dark') {
+    document.body.classList.add('dark-mode');
+  }
+});
